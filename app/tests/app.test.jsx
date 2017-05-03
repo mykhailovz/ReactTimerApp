c9b90e0235c7a9a0385326c3694868f197676ba4
Mykhailo <mykhailo.vz@gmail.com>
2017-05-03 08:29:57 -0400

    Add automatic tests

---
+++ app/tests/app.test.jsx
@@ -0,0 +1,10 @@
+var expect = require('expect');
+
+// for autocomplete
+var mocha = require('mocha');
+
+describe('App', () => {
+  it('should properly run tests', () => {
+    expect(1).toBe(1);
+  });
+});
